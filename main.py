c3f226db7e144d402f07f356d501835003467a65
david <david@melnicom.com>
2022-05-01 19:56:55 +0300

    v 1.2 added the function double

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def double(x):
+    return (x*2)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +13,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('david')
+    print_hi(f'david {double(3)}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
